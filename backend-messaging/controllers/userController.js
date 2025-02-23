cf55a4662e18a31567b582d95f60944713303bdd
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-02-23 13:11:56 -0500

    update: body data retrieved from validator matched data

--- backend-messaging/controllers/userController.js
+++ backend-messaging/controllers/userController.js
@@ -1,8 +1,9 @@
+const { matchedData } = require('express-validator');
 const userQueries = require('../prisma/queries/userQueries')
 
 async function getUserById(req, res, next) {
     try {
-        const userId = req.body.userId
+        const userId = matchedData(req, {locations: ['body'], onlyValidData: true});
         const user = await userQueries.getUserById(userId)
         return res.json({
             message: 'User retrieved successfully',
@@ -13,3 +14,19 @@ async function getUserById(req, res, next) {
         next(error)
     }
 }
+
+async function createUser(req, res, next) {
+    try {
+        const userData = matchedData(req, {locations: ['body'], onlyValidData: true})
+        //TODO: db query function
+        return res.json({
+            message: 'User created successfully',
+            data: user
+        });  
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}
+
+module.exports = {getUserById, createUser}
