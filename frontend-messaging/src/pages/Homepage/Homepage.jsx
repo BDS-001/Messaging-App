b2c903413340b10253e1ea1c2e78b099d5e5fa5e
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-02 15:03:58 -0500

    update: homepage redirects to login with no authentication

--- frontend-messaging/src/pages/Homepage/Homepage.jsx
+++ frontend-messaging/src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,15 @@
+import { useAuth } from '../../context/AuthContext';
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
 function Homapage() {
+        const navigate = useNavigate();
+        const {isAuth} = useAuth()
+    
+        useEffect(() => {
+            if (isAuth) navigate('/login')
+        }, [isAuth, navigate])
+
     return (
         <>
             HOMEPAGE
