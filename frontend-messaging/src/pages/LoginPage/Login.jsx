839d4b01c1b35472c1ee4b6b959ec934f9f405a8
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-02 13:10:10 -0500

    add: login page

---
+++ frontend-messaging/src/pages/LoginPage/Login.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+//TODO: change classnames to use module.css
+
+function LoginPage() {
+    const navigate = useNavigate();
+    const {login, isAuth} = useAuth()
+    const [loginData, setLoginData] = useState({email: '', password: ''});
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        if (isAuth) navigate('/')
+    }, [isAuth, navigate])
+
+    function handleChange(e) {
+        const {name, value} = e.target
+        setLoginData(prev => ({
+            ...prev, [name]: value
+        }))
+    }
+
+    function handleSubmit(e) {
+        setError(null)
+        e.preventDefault()
+        const loginSuccess = login(loginData)
+        if (!loginSuccess) setError('Incorrect Credentials, Try Again.')
+    }
+
+    return (
+        <>
+            {error && <div className="error-message">{error}</div>}
+            <div className="login-container">
+                <h2>Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" value={loginData.email} onChange={handleChange} required/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" value={loginData.password} onChange={handleChange} required/>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default LoginPage;
