159ec3af31452cce412978d7ef50bc46c71e4cee
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-03 11:37:05 -0500

    update: rename index.css

--- frontend-messaging/src/main.jsx
+++ frontend-messaging/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import routes from './routes/router'
-import './index.css'
+import './globalStyles.css'
 
 const router = createBrowserRouter(routes);
 createRoot(document.getElementById('root')).render(
