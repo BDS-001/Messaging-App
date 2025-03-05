2d62626ddad4125cd4080a7b3cd4b62899101bc7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-05 14:56:52 -0500

    update: password validatoin for create has improved security and allows more characters

--- backend-messaging/validators/userValidator.js
+++ backend-messaging/validators/userValidator.js
@@ -33,28 +33,33 @@ const userValidators = {
       
         // Password validation
         body('password')
-          .notEmpty()
-          .withMessage('Password is required')
-          .isLength({ min: 8 })
-          .withMessage('Password must be at least 8 characters long')
-          .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)
-          .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number and one special character')
-          .custom((value, { req }) => {
-            // Ensure password doesn't contain username or parts of email
-            const username = req.body.username?.toLowerCase();
-            const emailName = req.body.email?.split('@')[0].toLowerCase();
-            const lowercasePassword = value.toLowerCase();
-            
-            if (username && lowercasePassword.includes(username)) {
-              throw new Error('Password cannot contain your username');
-            }
-            
-            if (emailName && lowercasePassword.includes(emailName)) {
-              throw new Error('Password cannot contain your email username');
-            }
-            
-            return true;
-          }),
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%^&*()_\-+={\[\]}|<,>.?/])[A-Za-z\d~`!@#$%^&*()_\-+={\[\]}|<,>.?/]+$/)
+        .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
+        .custom((value) => {
+          // Check for dangerous characters or sequences
+          const dangerousPatterns = /<script>|javascript:|eval\(|document\.cookie|alert\(|<iframe>|<img|onerror=|href=|src=|['"\\]/i;
+          if (dangerousPatterns.test(value)) {
+            throw new Error('Password contains potentially dangerous characters or sequences that are not allowed (including quotes, backslashes and script-like patterns)');
+          }
+          return true;
+        })
+        .custom((value, { req }) => {
+          // Ensure password doesn't contain username or parts of email
+          const username = req.body.username?.toLowerCase();
+          const emailName = req.body.email?.split('@')[0].toLowerCase();
+          const lowercasePassword = value.toLowerCase();
+          if (username && lowercasePassword.includes(username)) {
+            throw new Error('Password cannot contain your username');
+          }
+          if (emailName && lowercasePassword.includes(emailName)) {
+            throw new Error('Password cannot contain your email username');
+          }
+          return true;
+        }),
         ],
         update: [
             // Validate URL parameter
