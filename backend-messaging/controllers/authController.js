042779929688c31957231e5238d02be1740b5d3b
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-07 19:42:49 -0500

    update: removed username from jwt

--- backend-messaging/controllers/authController.js
+++ backend-messaging/controllers/authController.js
@@ -36,7 +36,6 @@ async function login(req, res) {
             { 
                 id: user.id,
                 email: user.email,
-                username: user.username
             },
             process.env.JWT_SECRET,
             { 
