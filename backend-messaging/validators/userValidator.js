34096565988933826149415fe9212c2c573577cf
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-12 16:32:27 -0400

    update: validator error message

--- backend-messaging/validators/userValidator.js
+++ backend-messaging/validators/userValidator.js
@@ -123,7 +123,7 @@ const userValidators = {
               .notEmpty()
               .withMessage('Search term is required')
               .isLength({ min: 3, max: 30 })
-              .withMessage('Username must be between 3 and 30 characters')
+              .withMessage('Search must be between 3 and 30 characters')
           ],
 }
 
